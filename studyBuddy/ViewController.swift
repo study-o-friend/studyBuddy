0957b53d501ebe56e45614290a20a1daec7418e4
Ekko Lin <sihansolotop@gmail.com>
2017-03-21 17:03:22 -0400

    Initial Commit

---
+++ studyBuddy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  studyBuddy
+//
+//  Created by Ekko Lin on 3/21/17.
+//  Copyright © 2017 CodePath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
